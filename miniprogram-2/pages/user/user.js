db254312e0aa1ccbba79ca0f05c7047056a8c528
548534liulei <549201079@qq.com>
2019-11-19 15:38:44 +0800

    修改了全局样式app.json

--- miniprogram-2/pages/user/user.js
+++ miniprogram-2/pages/user/user.js
@@ -1,7 +1,7 @@
 // pages/user/user.js
 var app = getApp();
 Page({
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     let that = this;
     // console.log(e)
     // 获取用户信息
@@ -13,13 +13,13 @@ Page({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称
           wx.getUserInfo({
             success(res) {
-              console.log("获取用户信息成功", res)
               that.setData({
-                name: res.userInfo.nickName
+                info: res.userInfo,
+                isHidden: true
               })
             },
             fail(res) {
-              console.log("获取用户信息失败", res)
+              console.log("获取用户信息失败")
             }
           })
         } else {
@@ -32,8 +32,14 @@ Page({
   // onload:function(e){
   //   getUserInfo(opption);
   // }  
-data:{
-  img:"http://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574078958964&di=0e0d2c884be45eef375591f8d26fe321&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F424964918abcd1ef4e2179ef0ecadf544eaf26ce191f7-wKp6h7_fw658"
-}
+
+
+  data: {
+    img: "http://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574078958964&di=0e0d2c884be45eef375591f8d26fe321&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F424964918abcd1ef4e2179ef0ecadf544eaf26ce191f7-wKp6h7_fw658",
+    message: "需要您的授权才能使用哟",
+    info: [],
+    isHidden: false
+
+  }
 
 })
