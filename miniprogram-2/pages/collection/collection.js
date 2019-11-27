0c959baf516febd8da5cf600bebdc2ff23e552d2
liulei548534 <549201079@qq.com>
2019-11-27 16:40:29 +0800

    1127基本功能完善

--- miniprogram-2/pages/collection/collection.js
+++ miniprogram-2/pages/collection/collection.js
@@ -26,16 +26,19 @@ Page({
   onReady: function () {
 
   },
-  getPro(){
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
     //获取date
-    var date = app.globalData.date;
-    console.log(date)
+    // var date = app.globalData.date;
+    // console.log(date)
     //获取收藏缓存
     var detailStorage = wx.getStorageSync('isClolected');
-    console.log(detailStorage)
+    // console.log(detailStorage)
     var simpleDate = this.data.date
     if (detailStorage.length != 0) {
-      detailStorage.forEach((v, i) => true ? simpleDate.push(date[v.id]) : "")
+      detailStorage.forEach((v, i) => true ? simpleDate.push(app.globalData.date[v.id]) : "")
       this.setData({
         isHidden: true,
         date: simpleDate,
@@ -45,12 +48,6 @@ Page({
         isHidden:false,
       })
     }
-  },
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    this.getPro();
   },
 
   /**
@@ -88,51 +85,41 @@ Page({
 
   },
   coltClick(e){
+    // e.currentDa
+    // this.data.date[e.curre]
+   var index =  e.currentTarget.dataset.index
+   var date = this.data.date
+    date.splice(index,1)
     console.log("-----取消收藏-----")
-    var date = app.globalData.date;
-    var detailStorage = wx.getStorageSync('isClolected');
-    console.log('----------' + detailStorage)
-    var simpleDate = this.data.date
-    if (detailStorage.length != 0) {
-      detailStorage.forEach((v, i) => true ? simpleDate.push(date[v.id]) : "")
-      console.log(simpleDate.length)
-    }
-    var num = e.currentTarget.dataset.index;
-    var nums = num
-    var dete = this.data.date;
-    console.log("num:" + num)
-    var leng = simpleDate.length
-    console.log("leng:" + leng)
-    for (var i = 0; i < leng; i++) {
-      if (i === num) {
-        // 利用该方法来删除数组中元素（根据数据下标进行删除，1参数为删除几个）
-        console.log(i === num)
-        simpleDate.splice(i, 1)
-        num = -1;
-        console.log(num)
-      }
-    }
-    var indexs = this.data.indexs;
-    for (var i = 0; i < indexs.length; i++) {
-      if (i === nums + 1) {
-        indexs.splice(nums + 1, 1)
-        nums = -1;
-      }
-    }
     this.setData({
-      dete: simpleDate,
-      indexs
+      date
     })
-    console.log(this.data.date)
-    if (dete.length <= 0) {
-      this.setData({
-        isHidden: true
-      })
+    wx.setStorageSync('isClolected',date);
+    if(date.length==0){
+        this.setData({
+          isHidden:false,
+        })
     }
  },
-  addCarClick: function () {
+  addCarClick: function (e) {
     console.log("-----商品加入购物车-----")
+    var index =  e.currentTarget.dataset.index
+    var list = app.globalData.index
+    var date = this.data.date
+    // var count =0;
+    list.push(date[index])
+    date.splice(index,1)
+    this.setData({
+        date
+    })
+    wx.setStorageSync('isClolected',date);
+    if(date.length==0){
+      this.setData({
+        isHidden:false,
+      })
   }
+  },
+ 
 })
 
 
